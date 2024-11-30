ad55943422d5ca830ed383a1b76ddbc85ae00934
Ilhamlafeer <120272556+Ilhamlafeer@users.noreply.github.com>
2024-11-30 11:07:53 +0000

    Index file fetch data from table

---
+++ Public/index.php
@@ -0,0 +1,38 @@
+<?php
+// Get the db connection to file
+require_once 'dbconf.php';
+    try {
+        // Query
+        $sql = "SELECT * FROM students";
+
+        // Execute the query
+        $result = mysqli_query($connect, $sql);
+
+        // Check if data exists in the table
+        if (mysqli_num_rows($result) > 0) {
+            echo "<table border='1'>";
+            $col = mysqli_fetch_fields($result);
+            //Print the columns
+            echo "<tr>";
+            foreach ($col as $value) {
+                //return object
+                echo "<td>$value->name</td>";
+            }
+            echo "</tr>";
+
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>";
+                foreach ($row as $key => $value) {
+                    echo "<td>" . $value . "</td>";
+                }
+                echo "</tr>";
+            }
+            echo "</table>";
+        } else {
+            echo "No Results!";
+        }
+
+    } catch (Exception $e) {
+        die($e->getMessage());
+    }
+?>
