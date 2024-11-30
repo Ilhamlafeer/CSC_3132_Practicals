ab1832ca09416f311eb535d44263fa35c38860f3
Ilhamlafeer <120272556+Ilhamlafeer@users.noreply.github.com>
2024-11-30 12:05:36 +0000

    Commit undo

--- Public/2024_11_20/dbconf.php
+++ Public/2024_11_20/dbconf.php
@@ -1,15 +1,15 @@
 <?php
 //This is separate file to connect the database
 //constant variable
-$dbHost = getenv('DB_HOST');
-$dbName = getenv('DB_NAME');
-$dbUser = getenv('DB_USER');
-$dbPass = getenv('DB_PASS');
+define('SERVERNAME', '127.0.0.1:3306');
+define('USERNAME', 'root');
+define('PASSWORD', 'mariadb');
+define('DBNAME', 'students');
 
 //Use Try-Catch to find errors
 try{
     //connect with database
-    $connect = mysqli_connect($dbHost,$dbUser,$dbPass,$dbName);
+    $connect = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
 
     if(!$connect) {
         die("connection failed");
